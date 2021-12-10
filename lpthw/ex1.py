c50252148dc77aae665ac7a257a32c328aee09c4
uybixd <50666634+uybixd@users.noreply.github.com>
2021-12-10 17:47:47 +0800

    Update ex1.py

--- lpthw/ex1.py
+++ lpthw/ex1.py
@@ -1,1 +1,8 @@
-
+print("hello world!")
+print("Hello Again.")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd rather you 'not'.")
+print('I "said" do not touch this.')
+print('This is another line.')
