cfcaa7f76d42735715965622fa99cf5a1532d21c
uybixd <50666634+uybixd@users.noreply.github.com>
2021-12-13 16:07:57 +0800

    Create ex3_drill4.py

---
+++ lpthw/ex3_drill4.py
@@ -0,0 +1,24 @@
+# Rewrite ex3.py to use floating point numbers so it's more accurate. 20.0 is floating point.
+print("I will count my chickens:")
+
+print("Hens", 25.0 + 30 / 6)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Now I will count my eggs:")
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that is why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2)
+print("Is it less or equal?", 5.0 <= -2)
