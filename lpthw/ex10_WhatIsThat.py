a709a9d3d1d9ae24e7b1abecb16b352353abadb1
uybixd <50666634+uybixd@users.noreply.github.com>
2022-05-09 00:02:54 +0800

    Create ex10_WhatIsThat.py

---
+++ lpthw/ex10_WhatIsThat.py
@@ -0,0 +1,21 @@
+# assign tabby_cat =(equal) to a string which is start with \t
+tabby_cat = "\tI'm tabbed in." #tabby cat 虎斑猫
+# assign persian_cat =(equal) to a string which have a \n in it.
+persian_cat = "I'm split\non a line."
+# assign backslash_cat =(equal) to a string which hvae \\ to escape \ itself.
+backslash_cat = "I'm \\ a \\ cat."
+# assign fat_cat =(equal) to a triple-quoted string
+fat_cat = '''
+I'll do a lis
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t- Grass
+'''
+# print variable tabby_cat
+print(tabby_cat)
+# print variable persian_cat
+print(persian_cat)
+# print variable backslash_cat
+print(backslash_cat)
+# print variable fat_cat
+print(fat_cat)
