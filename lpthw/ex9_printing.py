06317575f915403a5521f6e89b837975efe00a03
uybixd <50666634+uybixd@users.noreply.github.com>
2022-05-08 23:53:39 +0800

    Create ex9_printing.py

---
+++ lpthw/ex9_printing.py
@@ -0,0 +1,19 @@
+# Here's some new strange stuff, remeber type it exactly.
+# assign a string to days
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# assign a stirng to months 
+# btw '\n' 应该也是print 的关键词 效果是后面的东西打印在下一行，如果后面没东西应该是光标在下一行。
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# print a string first, then print the variable days
+print("Here are the days: ",days)
+# print a string first, then print the variable months
+print("Here are the months: ", months)
+# I don't understand and I was shocked.
+# Why there are three double-quotes then we can print as much lines as we want?
+# print four lines of string in three double-quotes 
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
